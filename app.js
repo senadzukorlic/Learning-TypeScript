90d2999b02ef1b049f002c9acb1fe1314a950cb1
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-08-17 01:55:57 +0200

    tuple and enum types

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-// const person: { name: string; age: number } = {
+// const person: {
+//   name: string
+//   age: number
+//   hobbies: string[]
+//   role: [number, string]
+// } = {
+//   // const person = {
+//   name: "Senad",
+//   age: 23,
+//   hobbies: ["Sports", "Programing"],
+//   role: [2, "author"],
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 10] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Senad",
     age: 23,
     hobbies: ["Sports", "Programing"],
+    role: Role.ADMIN,
 };
+// person.role = [5, "dado"]
+// person.role[0] = 10  //tuple
 var favoriteActivities;
 favoriteActivities = ["Sports"];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
+}
+if (person.role === Role.ADMIN) {
+    console.log(Role.AUTHOR);
 }
--- app.ts
+++ app.ts
@@ -1,9 +1,32 @@
-// const person: { name: string; age: number } = {
+// const person: {
+//   name: string
+//   age: number
+//   hobbies: string[]
+//   role: [number, string]
+// } = {
+//   // const person = {
+//   name: "Senad",
+//   age: 23,
+//   hobbies: ["Sports", "Programing"],
+//   role: [2, "author"],
+// }
+
+enum Role { //enum type
+  ADMIN,
+  READ_ONLY,
+  AUTHOR = 10,
+}
+
 const person = {
   name: "Senad",
   age: 23,
   hobbies: ["Sports", "Programing"],
+  role: Role.ADMIN,
 }
+
+// person.role = [5, "dado"]
+
+// person.role[0] = 10  //tuple type
 
 let favoriteActivities: string[]
 
@@ -13,4 +36,7 @@ console.log(person.name)
 
 for (const hobby of person.hobbies) {
   console.log(hobby)
+}
+if (person.role === Role.ADMIN) {
+  console.log("is admin")
 }
