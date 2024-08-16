b5416e1309ac4d10e7a6bdba9338733ba814d504
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-08-16 15:58:24 +0200

    arrays types

--- app.js
+++ app.js
@@ -2,5 +2,12 @@
 var person = {
     name: "Senad",
     age: 23,
+    hobbies: ["Sports", "Programing"],
 };
+var favoriteActivities;
+favoriteActivities = ["Sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -2,6 +2,15 @@
 const person = {
   name: "Senad",
   age: 23,
+  hobbies: ["Sports", "Programing"],
 }
 
+let favoriteActivities: string[]
+
+favoriteActivities = ["Sports"]
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby)
+}
